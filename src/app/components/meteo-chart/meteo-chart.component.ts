30124114775c53edb0d0e3efa1735709e9e3ddb4
fre nzi <64012029+fresigns@users.noreply.github.com>
2023-10-10 09:46:21 +0200

    commit 2

--- src/app/components/meteo-chart/meteo-chart.component.ts
+++ src/app/components/meteo-chart/meteo-chart.component.ts
@@ -1,48 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js/auto';
+import { Forecast } from 'src/app/model/forecast';
+import { MeteoService } from 'src/app/services/meteo.service';
 
 @Component({
   selector: 'app-meteo-chart',
   templateUrl: './meteo-chart.component.html',
   styleUrls: ['./meteo-chart.component.scss']
 })
-export class MeteoChartComponent implements OnInit {
+export class MeteoChartComponent implements OnInit{
+chart: any;
 
-  public chart: any;
+  constructor(private mService: MeteoService){
 
+  }
   ngOnInit(): void {
-    this.createChart();
+    this.mService.getMeteoData().subscribe(data => {
+      // console.log(data);
+        this.initChart(data)
+
+    })
   }
 
-  createChart(){
-  
-    this.chart = new Chart("MyChart", {
-      type: 'bar', //this denotes tha type of chart
+  initChart(data: Forecast[]) {
 
-      data: {// values on X-Axis
-        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
-								 '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
-	       datasets: [
+    const ctx:any = document.getElementById('myChart');
+    const chart:any = new Chart(ctx,{
+      type: 'line',
+      data: {
+        labels: data.map(forecast => forecast.time.toISOString()),
+        datasets:[
           {
-            label: "Sales",
-            data: ['467','576', '572', '79', '92',
-								 '574', '573', '576'],
-            backgroundColor: 'blue'
+            label: 'precipitation probability',
+            data: data.map(forecast => forecast.precipitation),
+            borderWidth: 1
           },
           {
-            label: "Profit",
-            data: ['542', '542', '536', '327', '17',
-									 '0.00', '538', '541'],
-            backgroundColor: 'limegreen'
-          }  
-        ]
+            label:'humidity',
+            data: data.map(forecast => forecast.humidity),
+            borderWidth: 1,
+          },
+        ],
       },
       options: {
-        aspectRatio:2.5
+        scales: {
+          y: {
+            beginAtZero: true,
+            title:{
+              display:true,
+              text:'%'
+            }
+          }
+        }
       }
-      
     });
+    chart.canvas.parentNode.style.height = '500px';
   }
-
-
 }
